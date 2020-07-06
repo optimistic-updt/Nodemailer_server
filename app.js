12889db791395b197036a5df50e1a9ad741586b9
Kevi G <kevgarciamusic@hotmail.com>
2020-07-06 18:58:47 +1000

    changed cors domain name

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 var corsOptions = {
-  origin: 'https://www.kevgarcia.me',
+  origin: 'https://www.kevgarcia.fyi',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
